ec8ad29f319c9a46afcbeb19058e1b2d1a74735e
CSDEVELOPER\desig <parthsanghvi2811@gmail.com>
2024-08-05 18:44:45 +0530

    child theme added

---
+++ wp-content/themes/hello-theme-child/includes/classes/emails/class-wc-email-fun-questionnaire-order.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Class WC_Email_Fun_Questionnaire_Order file.
+ *
+ * @package WooCommerce\Emails
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( ! class_exists( 'WC_Email_Fun_Questionnaire_Order', false ) ) :
+
+	/**
+	 * Fun Questionnaire Order Email.
+	 *
+	 * An email sent to the customer when a order is fun questionnaire for.
+	 *
+	 * @class       WC_Email_Fun_Questionnaire_Order
+	 * @version     3.5.0
+	 * @package     WooCommerce\Classes\Emails
+	 * @extends     WC_Email
+	 */
+	class WC_Email_Fun_Questionnaire_Order extends WC_Email {
+
+		/**
+		 * Constructor.
+		 */
+		public function __construct() {
+			$this->id             = 'customer_fun_questionnaire_order';
+			$this->customer_email = true;
+
+			$this->title          = __( 'Fun Questionnaire', 'hello-elementor-child' );
+			$this->description    = __( 'An email sent to the customer when they place a custom order.', 'hello-elementor-child' );
+			$this->template_html  = 'emails/customer-fun-questionnaire-order.php';
+			$this->template_plain = 'emails/plain/customer-fun-questionnaire-order.php';
+			$this->placeholders   = array(
+				'{order_date}'   => '',
+				'{order_number}' => '',
+			);
+
+			// Call parent constructor.
+			parent::__construct();
+		}
+
+		/**
+		 * Get email subject.
+		 *
+		 * @since  3.1.0
+		 * @return string
+		 */
+		public function get_default_subject() {
+			return __( 'Your Order is ready for shipment', 'hello-elementor-child' );
+		}
+
+		/**
+		 * Get email heading.
+		 *
+		 * @since  3.1.0
+		 * @return string
+		 */
+		public function get_default_heading() {
+			return sprintf( _x( '[%s] Fun Questionnaire', 'default email subject for rental ggreement being sent to the customer', 'hello-elementor-child' ), '{blogname}' );
+		}
+
+		/**
+		 * Trigger the sending of this email.
+		 *
+		 * @param int            $order_id The order ID.
+		 * @param WC_Order|false $order Order object.
+		 */
+		public function trigger( $order_id, $order = false ) {
+			$this->setup_locale();
+
+			if ( $order_id && ! is_a( $order, 'WC_Order' ) ) {
+				$order = wc_get_order( $order_id );
+			}
+
+			if ( is_a( $order, 'WC_Order' ) ) {
+				$this->object                         = $order;
+				$this->recipient                      = $this->object->get_billing_email();
+				$this->placeholders['{order_date}']   = wc_format_datetime( $this->object->get_date_created() );
+				$this->placeholders['{order_number}'] = $this->object->get_order_number();
+			}
+
+			if ( $this->is_enabled() && $this->get_recipient() ) {
+				$this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
+			}
+
+			$this->restore_locale();
+		}
+
+		/**
+		 * Get content html.
+		 *
+		 * @return string
+		 */
+		public function get_content_html() {
+			return wc_get_template_html(
+				$this->template_html,
+				array(
+					'order'              => $this->object,
+					'email_heading'      => $this->get_heading(),
+					'additional_content' => $this->get_additional_content(),
+					'sent_to_admin'      => false,
+					'plain_text'         => false,
+					'email'              => $this,
+				)
+			);
+		}
+
+		/**
+		 * Get content plain.
+		 *
+		 * @return string
+		 */
+		public function get_content_plain() {
+			return wc_get_template_html(
+				$this->template_plain,
+				array(
+					'order'              => $this->object,
+					'email_heading'      => $this->get_heading(),
+					'additional_content' => $this->get_additional_content(),
+					'sent_to_admin'      => false,
+					'plain_text'         => true,
+					'email'              => $this,
+				)
+			);
+		}
+
+		/**
+		 * Default content to show below main email content.
+		 *
+		 * @since 3.7.0
+		 * @return string
+		 */
+		public function get_default_additional_content() {
+			return __( 'Thanks for using {site_url}!', 'hello-elementor-child' );
+		}
+	}
+
+endif;
+
+return new WC_Email_Fun_Questionnaire_Order();
