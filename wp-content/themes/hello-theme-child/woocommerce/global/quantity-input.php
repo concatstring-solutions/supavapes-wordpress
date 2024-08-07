e58a23084d5bb71c59c8c7c480194ea0ed9fee90
ParthSanghvi <parthsanghvi2811@gmail.com>
2024-08-07 13:46:55 +0530

    changes

--- wp-content/themes/hello-theme-child/woocommerce/global/quantity-input.php
+++ wp-content/themes/hello-theme-child/woocommerce/global/quantity-input.php
@@ -23,10 +23,10 @@ defined( 'ABSPATH' ) || exit;
 /* translators: %s: Quantity. */
 $label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 'woocommerce' ), wp_strip_all_tags( $args['product_name'] ) ) : esc_html__( 'Quantity', 'woocommerce' );
 
-$product = wc_get_product( $args['product_id'] );
-$backorders_allowed = $product->backorders_allowed();
-$stock_quantity = $product->get_stock_quantity();
-$max_value = $backorders_allowed ? '' : $stock_quantity;
+// $product = wc_get_product( $args['product_id'] );
+// $backorders_allowed = $product->backorders_allowed();
+// $stock_quantity = $product->get_stock_quantity();
+// $max_value = $backorders_allowed ? '' : $stock_quantity;
 
 ?>
 <div class="quantity">
@@ -50,7 +50,7 @@ $max_value = $backorders_allowed ? '' : $stock_quantity;
 		aria-label="<?php esc_attr_e( 'Product quantity', 'woocommerce' ); ?>"
 		size="4"
 		min="<?php echo esc_attr( $min_value ); ?>"
-        max="<?php echo esc_attr( $max_value ); ?>"
+        max=""
 		<?php if ( ! $readonly ) : ?>
 			step="<?php echo esc_attr( $step ); ?>"
 			placeholder="<?php echo esc_attr( $placeholder ); ?>"
